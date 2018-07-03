0419cf600d1acf1cb5f6aa8a61fcde05bb17e127
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-03 15:31:00 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,8 +3,9 @@ def my_each(array)
   if block_given?
     i = 0
     while i<array.length
-    i += 1
+      i += 1
+     end
+  else
+    puts "no valid input"
   end
-else
-  puts "no valid input"
 end
